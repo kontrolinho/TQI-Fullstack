9cf08541b60d6befb55a92e2a970ae69aaecd1c9
Hilton Gonçalves <100793952+kontrolinho@users.noreply.github.com>
2022-07-27 17:49:03 -0300

    Add files via upload

---
+++ Java/Variaveis tipos de dados e operadores matematicos/Conceituacao e Criacao/src/dio/Main.java
@@ -0,0 +1,16 @@
+package dio;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 30;
+        final int NUMERO_TENTATIVAS = 5;
+        int quantidadeProdutos = 25;
+
+        System.out.println(i);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(quantidadeProdutos);
+
+
+    }
+}
---
+++ Java/Variaveis tipos de dados e operadores matematicos/Tipos de dados/src/dio/main.java
@@ -0,0 +1,61 @@
+package dio;
+
+public class main {
+    public static void main(String[] args) {
+
+            byte b1 = 10;
+            byte b2 = 20;
+
+
+            // short s2 = 40000; Valor grande para short
+            short s1 = 20000;
+
+
+            // int i1 = -10000000000000000000000000000;
+            // valor grande para int
+            int i2 = 28500;
+
+
+            long l1 = 10000000000000L;
+            long l2 = 39700000000000L;
+
+
+            // float f1 = 4.5;
+            // num em float = f no final
+            float f2 = 10.86f;
+
+
+            // double d1 = 85.62;
+            // num em double = d no final
+            double d2 = 833.41d;
+
+
+            // char = character, apenas 1 caractere, utiliza-se
+            // aspas simples ''
+            char c1 = 'W';
+
+
+            // Strings sao textos, palavras, logo, utiliza-se
+            // aspas duplas ""
+            String st1 = "Palavra/Frase";
+            String st2 = "A321444%$#%$#2313...";
+
+            // Booleans = True/False
+            boolean bo1 = true;
+            boolean bo2 = false;
+
+            System.out.println(b1);
+            System.out.println(b2);
+            System.out.println(s1);
+            System.out.println(i2);
+            System.out.println(l1);
+            System.out.println(l2);
+            System.out.println(f2);
+            System.out.println(d2);
+            System.out.println(c1);
+            System.out.println(st1);
+            System.out.println(st2);
+            System.out.println(bo1);
+            System.out.println(bo2);
+    }
+}
